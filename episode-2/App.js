c44ce6a07bf0f4dfbecf207a4d48b35f256baec2
ashishxdev <ashishrana2004@gmail.com>
2025-09-24 18:27:01 +0530

    EP2

---
+++ episode-2/App.js
@@ -0,0 +1,19 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+const parent = React.createElement(
+    "div",
+    {id: "parent"},[
+    React.createElement("div",{id: "child"},[
+    React.createElement("h1",{},"I'm H1 Tag"), 
+    React.createElement("h2",{},"I'm H2 Tag")
+    ]),
+    React.createElement("div",{id: "child2"},[
+    React.createElement("h1",{},"I'm H1 Tag"), 
+    React.createElement("h2",{},"I'm H2 Tag")
+    ])
+])
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(parent)
