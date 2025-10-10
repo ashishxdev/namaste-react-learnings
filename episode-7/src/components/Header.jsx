9e211e4b5ceb16badb5336ad581f5915707c7d55
ashishxdev <ashishrana2004@gmail.com>
2025-10-10 14:02:51 +0530

    Lecture 6.1 & 6.2 & 7

---
+++ episode-7/src/components/Header.jsx
@@ -0,0 +1,38 @@
+import { LOGO_URL } from "../utils/constant";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+
+    const[btnNameReact, setbtnNameReact] = useState("Login")
+
+    // If no dependency array => useEffect is called on every render
+    // If dependency array is empty = [] => useEffect is called on initial render (just once)
+    // If dependency array is [btnNameReact] => called everytime btnNameReact is updated
+    useEffect(()=>{
+        console.log("useEffect Called")
+    }, [btnNameReact]);
+
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src={LOGO_URL}/>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About Us</Link></li>
+                    <li><Link to="/contact">Contact Us</Link></li>
+                    <li>Cart</li>
+                    <button className="login" onClick={()=>{
+                        btnNameReact == "Login"
+                        ? setbtnNameReact("Logout") 
+                        : setbtnNameReact("Login") 
+                        }}>{btnNameReact}</button>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
