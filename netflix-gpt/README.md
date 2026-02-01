b474970464126801088872a10cd90dc047e05aa1
ashishxdev <ashishrana2004@gmail.com>
2026-02-01 20:00:00 +0530

    changes

--- netflix-gpt/README.md
+++ netflix-gpt/README.md
@@ -3,10 +3,15 @@
 - Create React App
 - Configured Tailwind CSS
 - Header
+- Routing of Form
 - Login Form
 - Sign Up Form 
     - (If we have big form in project like we have 50-60 fields always use library like Formik)
-
+- Form Validation
+- useRef Hook
+- Firebase Setup
+- Deploying our app to production
+- Create Signup User Account
 
 # Features
 - Login/Signup
--- netflix-gpt/src/components/Login.jsx
+++ netflix-gpt/src/components/Login.jsx
@@ -1,8 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Header from "./Header";
+import { checkValidData } from "../utils/validate";
 
 const Login = () => {
   const [isSignInForm, setSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const name = useRef(null);
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const handleButtonClick = () => {
+    // Validate the form data
+
+    const message = checkValidData(isSignInForm ? null : name.current.value, email.current.value, password.current.value)
+    setErrorMessage(message);
+    if(message) return;
+
+    if(!isSignInForm){
+      // Sign Up Logic
+    }
+    else {
+      // Sign In Logi
+    }
+  }
 
   const toggleSignInForm = () => {
     setSignInForm(!isSignInForm);
@@ -17,28 +38,36 @@ const Login = () => {
           alt="bg"
         />
       </div>
-      <form className="w-3/12 my-36 mx-auto right-0 left-0 absolute p-12 bg-black text-white bg-opacity-80 rounded-lg">
+      <form 
+       onSubmit={(e) => e.preventDefault()}
+       className="w-3/12 my-36 mx-auto right-0 left-0 absolute p-12 bg-black text-white bg-opacity-80 rounded-lg">
         <h1 className="font-bold text-3xl">
           {isSignInForm ? "Sign In" : "Sign up"}
         </h1>
         {!isSignInForm && (
           <input
+            ref={name}
             type="text"
             placeholder="Full Name"
             className="p-4 my-4 w-full bg-[#393939] rounded-lg"
           />
         )}
         <input
+          ref={email}
           type="text"
           placeholder="Email Address"
           className="p-4 my-4 w-full bg-[#393939] rounded-lg"
         />
         <input
+          ref={password} 
           type="password"
           placeholder="Password"
           className="p-4 my-4 w-full bg-[#393939] rounded-lg"
         />
-        <button className="p-4 my-6 w-full bg-red-700 rounded-lg">
+        <p className="text-red-500 font-bold text-lg py-2">{errorMessage}</p>
+        <button className="p-4 my-6 w-full bg-red-700 rounded-lg"
+          onClick={handleButtonClick}
+          >
           Sign In
         </button>
         <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
